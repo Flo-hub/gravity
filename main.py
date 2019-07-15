d50b22430c23af8d65aa8aafd969b5e6a50be4e3
Flo-hub <52828520+Flo-hub@users.noreply.github.com>
2019-07-15 12:43:18 +0200

    mehr Planeten :)

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from system import System2d
 def main():
     p = []
     p.append(Particle((0, 0), (0, 0), 2e30))  # sun
-    p.append(Particle((-5.8e10, 0), (0, -47873), 3.3e23))  # mercury
-    p.append(Particle((1.5e11, 0), (0, 30000), 6e24))  # earth
+    p.append(Particle((-5.8e10, 0), (0, -47873), 3.3e23, 2.44e6))  # mercury
+    p.append(Particle((0,-1.1e11), (35021,0), 4.87e24, 6.10e6))  # venus
+    p.append(Particle((1.5e11, 0), (0, 30000), 5.97e24, 6.38e6))  # earth
+    p.append(Particle((3.48e8+1.5e11,0), (0,31000), 7.35e22, 3.48e6))  #moon
+    p.append(Particle((0,2.3e11), (-24131,0), 0.64e24, 3.40e6))  # mars
 
     system = System2d(p, 3600 * 24)
     # system.render()
