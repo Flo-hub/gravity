cb198af4821c4d3305ce24549f31fc22f08cf47c
Flo-hub <52828520+Flo-hub@users.noreply.github.com>
2019-07-19 16:01:39 +0200

    changes

--- particle.py
+++ particle.py
@@ -6,6 +6,8 @@ import pprint
 
 ## Fürs Code-Schreiben: limitiere Länge der Datensätze zu:
 
+
+
 class Point:
     
     def __init__(self, position, value = 0, heith = 0):
@@ -25,36 +27,33 @@ class Point:
         self.h += 1  # auf jeden Fall bin ich im Baum um 1 Stockwerk höher
         self.node = p
         if self.pos[p.h % d] < p.pos[p.h % d]:  # falls die i-te Komponente kleiner ist als die von p:
-            self.path.append(1)  # fûge ich meinem Weg einen links turn hinzu            
+            self.path.append(0)  # fûge ich meinem Weg einen links turn hinzu            
             if p.left != None:   # und entweder mache ich beim nâchsten Knoten weiter...
                 self.starts_climbing(p.left)  #...indem ich rekursiv das selbe tue
             else:   # oder es gibt keinen nächsten Knoten 
                 p.left = self  # und ich bleibe stehen
                 
         else:  # falls die i-te Komponente grösser ist agiere ich analog, diesmal für 
-            self.path.append(2)  # rechts
+            self.path.append(1)  # rechts
             if p.right != None: 
                 self.starts_climbing(p.right)
             else: 
                 p.right = self
-          
-    def distance_to(self,p):
+
+    def distance_to(self, p):
         r = self.pos - p.pos
-        print(str(p.pos))
-        return np.sqrt(np.sum(r ** 2 ))
-    
-    def distance_to_proj(self,p):
+        return np.sqrt(np.sum(r ** 2))
+
+    def distance_to_proj(self, p):
         d = len(self.pos)
-        r = self.pos[p.h/ % d]-p.pos[p.h/ % d]
+        r = self.pos[p.h  % d] - p.pos[p.h  % d]
         return np.absolute([r])
-        
-    def ancestors(self,list):
+
+    def ancestors(self, list):
         if self.node != None:
             list.append(self.node)
             self.node.ancestors(list)
         return list
-    
-    
             
            
                 
@@ -92,138 +91,172 @@ class Tree:
         return distances
                   
         
-    def closest_of(self,point,k):
-        
-        
+    def neighbors_of(self,point,k):
+
         self.grows(point)
-        
-        neighbours = sorted(point.ancestors([]), key = point.distance_to())
-        
-        if len(neighbours) > k:
-            for i in range(len(neighbours)-k):
-                neighbours.remove(neighbours[k-1+i])
-         
-        for p in neighbours:
-            if point.distance_to_proj(p) < point.distance_to(neighbours[k-1]):
-                for q in self.points:
-                    if len(q.path) > p.h + 1 and q.path[p.h + 1] = (point.path[p.h + 1] + 1) % 2 :
-                            if point.distance_to(q) < point.distance_to(neighbours[k-1] and len(neighbours) = k:
-                                neighbours = sorted(neighbours.append(q))
-                                neighbours.remove(neighbours[k])
-                            elif  len(neighbours) < k:
-                                neighbours.append(q)
-                        
-        self.points.remove(point)                             
-        return neighbours
-        
-    
+        print('')
+        print('')
+        neighbors = sorted( point.ancestors([]), key = point.distance_to )
+        for p in neighbors:
+            print( str(p.pos) + ': ' + str(point.distance_to(p)) )
+        print('')
 
+        #print(str(neighbors))
         print('')
-        for p in self.points:
-            print(str(p.path))
-        
-        print('')
-        
-        dist = sorted(self.node_distances(point,point,[]))  # step 1
-        #print(str(dist))
-       
-        
+
+        if len(neighbors) > k:
+            for i in range(len(neighbors) - k):
+                neighbors.remove(neighbors[len(neighbors)-1])
             
+        print('gekürzt')
+        for p in neighbors:
+            print( str(p.pos) + ': ' + str(point.distance_to(p)) )    
+            
+
+        for p in neighbors:
+            if point.distance_to_proj(p) < point.distance_to(neighbors[k - 1]):
+                print('Es gibt vielleicht Kandidaten beim anderen Zweig des Knoten nr ' + str(p.h))
+                for q in self.points:
+                    if q.h > p.h and (q.path[:p.h-1] == point.path[:p.h-1]  and q.path[p.h] == (point.path[p.h] + 1) % 2):
+                        if len(neighbors) == k:
+                            if point.distance_to(q) < point.distance_to(neighbors[k - 1]):
+                                neighbors.append(q)
+                                neighbors = sorted(neighbors, key = point.distance_to)
+                                neighbors.remove(neighbors[k])
+                        elif len(neighbors) < k:
+                            neighbors.append(q)
+            for p in neighbors:
+                print( str(p.pos) + ': ' + str(point.distance_to(p)) )
+            print('')
+
+        self.points.remove(point)
+        print('Die ' + str(k) + ' nächsten Nachbarn sind:')
         
-        else:
-            return dist
+        return neighbors
         
+      
         
-        print('')
-        for p in self.points:
-            print(str(p.path))        
+    
+
+
     
 
 
 
 
+#tree = Tree(2)
+
+#tree.grows(Point([3,3]))
+#tree.grows(Point([4,2]))
+#tree.grows(Point([5,3]))
+#tree.grows(Point([5,1]))
+#tree.grows(Point([3,1]))
+#tree.grows(Point([1,2]))
+#tree.grows(Point([1,1]))
+#tree.grows(Point([2,3]))
+#tree.grows(Point([5,3/2]))
+
+#k = 3
+
+#for p in tree.neighbors_of(Point([4,3/2]),k):
+#    print(str(p.pos))
 
 
-tree = Tree(2)
 
-tree.grows(Point([3,3]))
-tree.grows(Point([4,2]))
-tree.grows(Point([5,3]))
-tree.grows(Point([5,1]))
-tree.grows(Point([3,1]))
-tree.grows(Point([1,2]))
-tree.grows(Point([1,1]))
-tree.grows(Point([2,3]))
-
-print(tree.closest_of(Point([4,3/2])))
-
-print('')
-print()
+#def data_points(list):
+ #   for 
+    
 
   
- #Wir brauchen:
-d
-l                                                                       
-l1 = 
-l2 =                                                                     
-train_set = 
-trees = []
-values = []     
-sign()  
-best = 1                                                                        
-K =     
-
-for k in KSET:
-    Error = 0                                                                       
+def classify(name, l = 5):
+    
+    t1 = process_time()
+    
+    data_set = np.genfromtxt("{}.train.csv".format(name) , delimiter=',')
+    
+    d = len(data_set[0])-1                                                                    
+    n = len(data_set)
+    if n % l == 0:  # Falls n durch l teilbar ist, erhalten wir exakt gleich große Teildatensätze.
+        l1 = int(n / l)
+        l2 = l1
+    else:           # Falls n nicht durch l teilbar ist, kürzen wir den letzten Teildatensatz
+        l1 = math.ceil(n/l) - 1         # Anzahl der Datenpunkte in den ersten (l-1) Blocks
+        l2 = math.ceil(n/l) - 1 + n % l   
+                                                                 
+    trees = []
+    
     for r in range(l-1):
-        R = 0                                                                  
         trees.append(Tree(d))                                                                        
         for v in data_set[:r*l1]:
             trees[r].grows(Point(v[1:],v[0]))                                                              
         for v in data_set[(r+1)*l1:]:
             trees[r].grows(Point(v[1:],v[0]))
-        for w in data_set[r*l1:(r+1)*l1]:
-            value = 0
-            for p in trees[r].closest_of(Point(w[1:],w[0]),k):
-                value += p.value
-            if sign(value) != w[0]:
-                R += 1                                                                
-            values.append(sign(value))   
-        R /= l1  
-        Error += R                                                                    
-    R = 0                                                                        
     trees.append(Tree(d))                                                                        
-        for v in data_set[:(l-1)*l1]:
-            trees[r].grows(Point(v[1:],v[0]))                                                            
+    for v in data_set[:(l-1)*l1]:
+        trees[l-1].grows(Point(v[1:],v[0]))  
+    
+    
+    values = []     
+    best = 1                                                                        
+    K = None
+
+    for k in range(1,201):
+        Error = 0                                                                       
+        for r in range(l-1):
+            R = 0                                                                  
+            for w in data_set[r*l1:(r+1)*l1]:
+                value = 0
+                for p in trees[r].closest_of(Point(w[1:],w[0]),k):
+                    value += p.value
+                if sign(value) != w[0]:
+                    R += 1                                                                
+                values.append(sign(value))   
+            R /= l1  
+            Error += R                                                                    
+        R = 0                                                                        
         for w in data_set[(l-1)*l1:]:
             value = 0
-            for p in trees[r].closest_of(Point(w[1:],w[0]),k):
+            for p in trees[l-1].closest_of(Point(w[1:],w[0]),k):
                 value += p.value
-             if sign(value) != w[0]:
-                R += 1                                                               
-            values.append(sign(value))                                                                                                                                          
-    R /= l2                                                                       
-    Error =+ R                                                                                                                                               
-    Error /= l                                                                  
-    if Error < best:
-        K = k                                                                    
+                if sign(value) != w[0]:
+                    R += 1                                                               
+                values.append(sign(value))                                                                                                                                          
+        R /= l2                                                                       
+        Error += R                                                                                                                                               
+        Error /= l                                                                  
+        if Error < best:
+            K = k                                                                    
                                                                                                                                                 
                                                                         
-test_set =                                                                         
-classifications = []
+    test_set =  np.genfromtxt("{}.test.csv".format(name) , delimiter=',')     
+                                                                
+    classifications = []
+    R = 0                                                                    
+    for v in test_set:                                                                    
+        classification = 0                                                                    
+        for r in range(l):
+            value = 0                                                                
+            for p in trees[r].closest_of(Point(v[1:],v[0]),K):                                                                
+                value += p.value   
+                value = sign(value)  
+            classification += value 
+        classification = sign(classification)
+        if classification != v[0]:
+            R += 1
+        classifications.append(sign(classification))  
+    R /= len(test_set) 
+    
+    t2 = process_time()
+    print('Best number of neighbors: {}'.format(K))
+    print('Ratio of failure: {} %'.format(100*R))
+    print('Performance: {} s'.format(t2-t1))
+                                                           
                                                                         
-for v in test_set:                                                                    
-    classification = 0                                                                    
-    for r in range(l):
-        value = 0                                                                
-        for p in trees[r].closest_of(Point(v[1:],v[0])):                                                                
-            value += p.value   
-        value = sign(value)  
-        classification += value                                                                
-    classifications.append(sign(classification))                                                               
                                                                         
-                                                                        
-                                                                        
+   #Just_do_it.py
+from Classify import classify
+
+classify('bananas1-2d')                                                                     
                                                                         
                                                                         
                                                                         
